08297f2f3d914cf22c48b51b9f229ca1fd851ce4
it2konst <gorky17@tuta.io>
2024-10-17 17:55:52 +0700

    first commit

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,52 @@
+<script setup>
+const props = defineProps({
+    imgUrl: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="root">
+        <div :style="{ backgroundImage: `url(${props.imgUrl})` }" class="img"></div>
+        <div class="main">
+            <el-button class="btn">Get random cocktail</el-button>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+
+.root
+    display: flex
+    min-height: 100vh
+    background-color: $background
+
+.img
+    width: 50%
+    background-repeat: no-repeat
+    background-position: 50% 50%
+    background-size: cover
+
+.main
+    position: relative
+    width: 50%
+    padding: 32px 40px
+
+.btn
+    position: absolute
+    top: 32px
+    right: 40px
+    font-size: 16px
+    font-family: 'Raleway', 'Arial', sans-serif
+    background-color: $accent
+    background-color: $accent
+    color: $text
+
+    &:hover, &:active
+        background-color: $accent-dark
+        background-color: $accent-dark
+</style>
---
+++ src/pages/Cocktail.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    Cocktail
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+</style>
