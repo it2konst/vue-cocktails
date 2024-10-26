9becb4e379912972e5c6179ba5077db100587e0a
it2konst <gorky17@tuta.io>
2024-10-27 06:04:58 +0700

    API getCocktail and ingredients added

--- src/components/CocktailThumb.vue
+++ src/components/CocktailThumb.vue
@@ -21,11 +21,11 @@ const props = defineProps({
 
 .root
     width: calc((100% - 76px) / 3)
-    padding-right: 38px
+    margin-right: 38px
     padding-bottom: 20px
 
     &:nth-child(3n + 3)
-        padding-right: 0
+        margin-right: 0
 
 .pic
     width: 120px
@@ -39,4 +39,8 @@ const props = defineProps({
 .name
     padding-top: 15px
     letter-spacing: 0.1rem
+    width: 100px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
 </style>
